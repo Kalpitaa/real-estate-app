86097d3c4d778e2d41b1e679a68832de11c72006
kalpitaa <mgkalpitaa@gmail.com>
2026-05-24 22:57:10 +0530

    add cors headers manually

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,16 @@ dotenv.config();
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 // Middleware
 app.use(cors({
   origin: [
